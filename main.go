92db074324655e0d1e9e6c7e30598e39a02d0b22
gericass <bonjovi1926@yahoo.co.jp>
2018-05-15 16:24:08 +0900

    push

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/c-bata/rtmp"
+)
+
+func main() {
+	var addr string
+	flag.StringVar(&addr, "addr", ":1935", `proxy local address`)
+	flag.Parse()
+
+	log.Printf("Serving RTMP on %s", addr)
+	err := rtmp.ListenAndServe(addr)
+	if err != nil {
+		log.Fatalf("Catch Error: %s", err)
+	}
 }
